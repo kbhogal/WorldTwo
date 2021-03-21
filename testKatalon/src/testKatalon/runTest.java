25ab3d3e2e04e1d35ad2ce913071e72cb69ef88a
kirpal <kirpal@192-168-1-128.tpgi.com.au>
2021-03-21 21:22:35 +1100

    First project

---
+++ testKatalon/src/testKatalon/runTest.java
@@ -0,0 +1,16 @@
+package testKatalon;
+
+public class runTest {
+
+	public runTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello People");
+
+	}
+
+}
